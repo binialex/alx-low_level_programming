3ee9e43cefad498d6fae7b389c4dcc0dd48d583a
binialex <bibina3413@gmail.com>
2021-02-05 18:36:02 +0000

    c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-prints numbers
+ * Return: 0
+ */
+int main(void)
+{
+  int a = 0;
+  while (a < 10)
+    {
+      printf("%d", a);
+      a++;
+    }
+  printf("\n");
+  return (0);
+}
+
