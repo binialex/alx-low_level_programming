75ffe3dbb8c0091cb0939eec356b8566884d0d61
binialex <bibina3413@gmail.com>
2021-02-05 18:08:13 +0000

    c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+  char x = 'a';
+  while (x <= 'z')
+    {
+      if (x != 'e' && x != 'q')
+	putchar(x);
+      x++;
+    }
+  putchar ('\n');
+  return (0);
+}
